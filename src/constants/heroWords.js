586124339998d968024889cca6be41d5424f9639
AsJayTee <jinthauofficial@gmail.com>
2025-04-16 22:01:16 +0800

    Changed some hero words

--- src/constants/heroWords.js
+++ src/constants/heroWords.js
@@ -7,5 +7,5 @@ export const words = [
   { text: "Data Pipelines", imgPath: "/images/hero/data_pipelines.png" },
   { text: "NLP", imgPath: "/images/hero/nlp.png" },
   { text: "Generative AI", imgPath: "/images/hero/generative_ai.png" },
-  { text: "Some Caffeine", imgPath: "/images/hero/some_caffeine.png" }
+  { text: "Not Enough Sleep", imgPath: "/images/hero/net_enough_sleep.png" }
 ];
