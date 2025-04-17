3f64d472c806f0ae0fc9647f369a71edd93a5518
AsJayTee <jinthauofficial@gmail.com>
2025-04-17 12:50:43 +0800

    Added navlinks constant

---
+++ src/constants/navLinks.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    name: "Projects",
+    link: "#projects",
+  },
+  {
+    name: "Experience",
+    link: "#experience",
+  },
+  {
+    name: "Skills",
+    link: "#skills",
+  },
+  {
+    name: "Testimonials",
+    link: "#testimonials",
+  },
+];
