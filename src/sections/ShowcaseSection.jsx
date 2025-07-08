c6be1bc20055597c8b4a937a69c5ab6a63fd0cd4
AsJayTee <jinthauofficial@gmail.com>
2025-07-08 23:30:59 +0800

    Implementation: Added TitleHeader components and included in showcase section

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react'
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useGSAP } from '@gsap/react';
+import TitleHeader from "../components/TitleHeader";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -41,54 +42,57 @@ const ShowcaseSection = () => {
   }, []);
 
   return (
-    <section id="projects" className="app-showcase" ref={sectionRef}>
-      <div className="w-full">
-        <div className="showcaselayout">
-          {/* LEFT */}
-          <div className="first-project-wrapper" ref={project1Ref}>
-            <div className="image-wrapper">
-              <img src="images\showcase\pb_chatbot.png" alt="Psychology Blossom Chatbot" />
-            </div>
-            <div className="text-content">
-              <h2>
-                Business Enquiries Made Simple with a Reliable, Robust, and Responsive Chatbot
-              </h2>
-              <p className="text-white-50 md:text-xl">
-                A full-stack project powered by JavaScript, Python, and Google Cloud, 
-                designed for fast and seamless user engagement.
-                Check it out here: {" "}
-                <a href="https://psychologyblossom.com/" className="underline decoration-[1px] hover:text-white">
-                  Live Chatbot
-                </a> 
-                {" "} | {" "}
-                <a href="https://github.com/AsJayTee/PB-Chatbot" className="underline decoration-[1px] hover:text-white">
-                  GitHub Repo
-                </a>
-              </p>
-            </div>
-          </div>
-          {/* RIGHT */}
-          <div className="project-list-wrapper overflow-hidden">
-            <div className="project" ref={project2Ref}>
-              <div className="image-wrapper bg-[#dbffdc]">
-                <img src="images\showcase\3d_ai_avatar.png" alt="3D Avatar Waving" />
+    <div>
+      <TitleHeader title={"My Projects"} sub={"Things I Build 🔧"} />
+      <section id="projects" className="app-showcase" ref={sectionRef}>
+        <div className="w-full">
+          <div className="showcaselayout">
+            {/* LEFT */}
+            <div className="first-project-wrapper" ref={project1Ref}>
+              <div className="image-wrapper">
+                <img src="images\showcase\pb_chatbot.png" alt="Psychology Blossom Chatbot" />
               </div>
-              <h2>
-                Interactive 3D AI Avatars
-              </h2>
-            </div>
-            <div className="project" ref={project3Ref}>
-              <div className="image-wrapper bg-[#e7e7ff]">
-                <img src="images\showcase\fraud_analysis.png" alt="ROC Curve" />
+              <div className="text-content">
+                <h2>
+                  Business Enquiries Made Simple with a Reliable, Robust, and Responsive Chatbot
+                </h2>
+                <p className="text-white-50 md:text-xl">
+                  A full-stack project powered by JavaScript, Python, and Google Cloud, 
+                  designed for fast and seamless user engagement.
+                  Check it out here: {" "}
+                  <a href="https://psychologyblossom.com/" className="underline decoration-[1px] hover:text-white">
+                    Live Chatbot
+                  </a> 
+                  {" "} | {" "}
+                  <a href="https://github.com/AsJayTee/PB-Chatbot" className="underline decoration-[1px] hover:text-white">
+                    GitHub Repo
+                  </a>
+                </p>
               </div>
-              <h2>
-                Fraud in Electricity and Gas Consumption
-              </h2>
+            </div>
+            {/* RIGHT */}
+            <div className="project-list-wrapper overflow-hidden">
+              <div className="project" ref={project2Ref}>
+                <div className="image-wrapper bg-[#dbffdc]">
+                  <img src="images\showcase\3d_ai_avatar.png" alt="3D Avatar Waving" />
+                </div>
+                <h2>
+                  Interactive 3D AI Avatars
+                </h2>
+              </div>
+              <div className="project" ref={project3Ref}>
+                <div className="image-wrapper bg-[#e7e7ff]">
+                  <img src="images\showcase\fraud_analysis.png" alt="ROC Curve" />
+                </div>
+                <h2>
+                  Fraud in Electricity and Gas Consumption
+                </h2>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   )
 }
 export default ShowcaseSection
