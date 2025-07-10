0ca2172592ce7da2288cf8cf746a0a70c266e535
AsJayTee <jinthauofficial@gmail.com>
2025-07-10 10:58:21 +0800

    Improvement: Project animation timing on mobile

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -29,7 +29,7 @@ const ShowcaseSection = () => {
           delay: 0.3 * (index + 1),
           scrollTrigger: {
             trigger: card,
-            start: "top bottom-=100"
+            start: "top bottom-=30"
           }
         }
       )
