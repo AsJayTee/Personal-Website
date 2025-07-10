add5c05711eb807c46743ed46bc3934c2eacd12b
AsJayTee <jinthauofficial@gmail.com>
2025-07-10 12:44:08 +0800

    Implementation: Update Experience Section

---
+++ src/constants/experiences.js
@@ -0,0 +1,14 @@
+export const experiences = [
+  {
+    companyDescription: "The largest telecom company in Malaysia, serving 20M+ users with a workforce of around 3,000. ",
+    imgPath: "/images/exp1.png",
+    logoPath: "/images/logo1.png",
+    title: "AI Engineer",
+    date: "May 2024 - August 2024",
+    responsibilities: [
+      "Develop and implement machine learning models and algorithms.",
+      "Design AI applications tailored to specialised business problem statements.",
+      "Conduct data preprocessing and analysis to support AI projects.",
+    ],
+  },
+];
--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader'
+import { experiences } from '../constants/experiences'
 
 const ExperienceSection = () => {
   return(
-    <section id="experience" className="w-full md:mt-10 mt-10 section-padding xl:px-0">
+    <section id="experience" className="w-full md:mt-10 mt-10 px-5 md:px-20 py-10 md:py-20">
       <div className="w-full overflow-hidden">
         <TitleHeader
           title={"Professional Work Experience"}
           sub={"Things I did 💼"}
         />
+        <div className="mt-32 relative">
+          <div className="relative z-50 xl:space-y-32 space-y-10">
+            { experiences.map((card, index) => (
+              <div key={card.title} className="exp-card-wrapper text-white"> 
+                {card.title}
+              </div>
+            )) }
+          </div>
+        </div>
       </div>
     </section>
   )
