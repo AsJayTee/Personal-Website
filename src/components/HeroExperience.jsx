1f750c6a12ad2e8109900df087eb98d73d3c6598
AsJayTee <jinthauofficial@gmail.com>
2025-07-10 11:09:46 +0800

    Bugfix: Loader blocking TitleHeader underneath

--- src/components/HeroExperience.jsx
+++ src/components/HeroExperience.jsx
@@ -142,7 +142,7 @@ const HeroExperience = () => {
           top: 0,
           left: 0,
           right: 0,
-          bottom: 0,
+          bottom: '5%',
           backgroundColor: '#171720',
           display: 'flex',
           justifyContent: 'center',
