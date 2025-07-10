ce271409a56e6cca632923d0076aea3a25e5a2c9
AsJayTee <jinthauofficial@gmail.com>
2025-07-10 12:05:19 +0800

    Implementation: Added feature cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./sections/Hero"
 import Navbar from "./sections/Navbar"
 import ShowcaseSection from "./sections/ShowcaseSection"
+import FeatureCards from "./sections/FeatureCards"
 import ExperienceSection from "./sections/ExperienceSection"
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <ShowcaseSection />
+      <FeatureCards />
       <ExperienceSection />
     </>
   )
