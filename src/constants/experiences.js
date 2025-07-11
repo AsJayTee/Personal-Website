d61b02a01103439f8468f3a8d5342c85fd09e522
AsJayTee <jinthauofficial@gmail.com>
2025-07-11 10:47:36 +0800

    Improvement: Changed listed data in experiences

--- src/constants/experiences.js
+++ src/constants/experiences.js
@@ -1,6 +1,6 @@
 export const experiences = [
   {
-    companyDescription: "The largest telecom company in Malaysia, serving 20M+ users with a workforce of around 3,000. ",
+    companyName: "CelcomDigi",
     logoPath: "/images/experiences/celcomdigi.png",
     title: "AI Engineer",
     date: "May 2024 - August 2024",
