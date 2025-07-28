2c207898bce100f8a55dc8cab945b6aa89f08c06
AsJayTee <jinthauofficial@gmail.com>
2025-07-28 15:02:29 +0800

    Implementation: Added attributions file

---
+++ src/constants/attributions.js
@@ -0,0 +1,150 @@
+export const attributions = [
+    {
+        "3D Models": [
+            {
+                "Library": {
+                    "name": ["Unlucky Library", "https://sketchfab.com/3d-models/dae-simple-scene-unlucky-library-7f09861579394ad686cf6150736281d9"],
+                    "created-by": ["Bee Badea", "https://sketchfab.com/beebadea"],
+                    "licensed-under": ["CC-BY-4.0", "https://creativecommons.org/licenses/by/4.0/"]
+                }
+            }
+        ]
+    },
+    {
+        "Fonts & Typography": [
+            {
+                "Mona Sans": {
+                    "source": ["Github's Mona Sans", "https://github.com/github/mona-sans"],
+                    "licensed-under": ["Open Source Font Family"]
+                }
+            }
+        ]
+    },
+    {
+        "Icons": [
+            {
+                "Favicon": {
+                    "path": "/images/favicon/terminal.png",
+                    "link": "https://www.flaticon.com/free-icon/terminal_10478244",
+                    "created-by": ["HideMaru - Flaticon", "https://www.flaticon.com/authors/hidemaru"]
+                }
+            },
+            {
+                "Hero Words Slider": [
+                    {
+                        "path": "/images/hero/code.png",
+                        "link": "https://www.flaticon.com/free-icon/code_984196",
+                        "created-by": ["Freepik - Flaticon", "https://www.flaticon.com/authors/freepik"]
+                    },
+                    {
+                        "path": "/images/hero/data_pipelines.png",
+                        "link": "https://www.flaticon.com/free-icon/data-processing_10158758",
+                        "created-by": ["orvipixel - Flaticon", "https://www.flaticon.com/authors/orvipixel"]
+                    },
+                    {
+                        "path": "/images/hero/data.png",
+                        "link": "https://www.flaticon.com/free-icon/database_5816594",
+                        "created-by": ["rukanicon - Flaticon", "https://www.flaticon.com/authors/rukanicon"]
+                    },
+                    {
+                        "path": "/images/hero/deep_learning.png",
+                        "link": "https://www.flaticon.com/free-icon/deep-learning_12538668",
+                        "created-by": ["Freepik - Flaticon", "https://www.flaticon.com/authors/freepik"]
+                    },
+                    {
+                        "path": "/images/hero/generative_ai.png",
+                        "link": "https://www.flaticon.com/free-icon/generative_16806607",
+                        "created-by": ["LAFS - Flaticon", "https://www.flaticon.com/authors/lafs"]
+                    },
+                    {
+                        "path": "/images/hero/nlp.png",
+                        "link": "https://www.flaticon.com/free-icon/language_9828893",
+                        "created-by": ["Freepik - Flaticon", "https://www.flaticon.com/authors/freepik"]
+                    },
+                    {
+                        "path": "/images/hero/not_enough_sleep.png",
+                        "link": "https://www.flaticon.com/free-icon/coffee_818217",
+                        "created-by": ["Freepik - Flaticon", "https://www.flaticon.com/authors/freepik"]
+                    },
+                    {
+                        "path": "/images/hero/passion.png",
+                        "link": "https://www.flaticon.com/free-icon/passion_7172930",
+                        "created-by": ["juicy_fish - Flaticon", "https://www.flaticon.com/authors/juicy-fish"]
+                    },
+                    {
+                        "path": "/images/hero/statistics.png",
+                        "link": "https://www.flaticon.com/free-icon/chart_900784",
+                        "created-by": ["srip - Flaticon", "https://www.flaticon.com/authors/srip"]
+                    }
+                ]
+            },
+            {
+                "Feature Cards": [
+                    {
+                        "path": "/images/features/chat.png",
+                        "link": "https://www.flaticon.com/free-icon/chat_811476",
+                        "created-by": ["dmitri13 - Flaticon", "https://www.flaticon.com/authors/dmitri13"]
+                    },
+                    {
+                        "path": "/images/features/quality.png",
+                        "link": "https://www.flaticon.com/free-icon/favorites_7601227",
+                        "created-by": ["gungyoga04 - Flaticon", "https://www.flaticon.com/authors/gungyoga04"]
+                    },
+                    {
+                        "path": "/images/features/time.png",
+                        "link": "https://www.flaticon.com/free-icon/chronometer_850960",
+                        "created-by": ["Freepik - Flaticon", "https://www.flaticon.com/authors/freepik"]
+                    }
+                ]
+            },
+            {
+                "Skill Cards": [
+                    {
+                        "path": "images/skills/main/ai.png",
+                        "link": "https://www.flaticon.com/free-icon/artificial-intelligence_901002",
+                        "created-by": ["Freepik - Flaticon", "https://www.flaticon.com/authors/freepik"]
+                    },
+                    {
+                        "path": "images/skills/main/cloud.png",
+                        "link": "https://www.flaticon.com/free-icon/cloud-service_3211343",
+                        "created-by": ["Freepik - Flaticon", "https://www.flaticon.com/authors/freepik"]
+                    },
+                    {
+                        "path": "images/skills/main/database.png",
+                        "link": "https://www.flaticon.com/free-icon/database_1104982",
+                        "created-by": ["srip - Flaticon", "https://www.flaticon.com/authors/srip"]
+                    },
+                    {
+                        "path": "images/skills/main/languages.png",
+                        "link": "https://www.flaticon.com/free-icon/python_5968350",
+                        "created-by": ["Freepik - Flaticon", "https://www.flaticon.com/authors/freepik"]
+                    },
+                    {
+                        "path": "images/skills/main/web.png",
+                        "link": "https://www.flaticon.com/free-icon/world-wide-web_1006669",
+                        "created-by": ["Freepik - Flaticon", "https://www.flaticon.com/authors/freepik"]
+                    }
+                ]
+            },
+            {
+                "Footer": [
+                    {
+                        "path": "/images/footer/email.png",
+                        "link": "https://www.flaticon.com/free-icon/mail_646094",
+                        "created-by": ["Freepik - Flaticon", "https://www.flaticon.com/authors/freepik"]
+                    },
+                    {
+                        "path": "/images/footer/github.png",
+                        "link": "https://www.flaticon.com/free-icon/github_1051377",
+                        "created-by": ["Freepik - Flaticon", "https://www.flaticon.com/authors/freepik"]
+                    },
+                    {
+                        "path": "/images/footer/linkedin.png",
+                        "link": "https://www.flaticon.com/free-icon/linkedin_1384088",
+                        "created-by": ["Freepik - Flaticon", "https://www.flaticon.com/authors/freepik"]
+                    }
+                ]
+            }
+        ]
+    }
+]
